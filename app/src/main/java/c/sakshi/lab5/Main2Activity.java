0b31a6d43a8138ba56084dc502651b598c03a314
Peter <pswanson2@wisc.edu>
2020-02-26 15:20:34 -0600

    Made some progress on milestone 1

---
+++ app/src/main/java/c/sakshi/lab5/Main2Activity.java
@@ -0,0 +1,20 @@
+package c.sakshi.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+//        t = (TextView) findViewById(R.id.textView);
+//        String s = getIntent().getStringExtra("message");
+//        t.setText(s);
+    }
+}
--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,9 +1,25 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText e = (EditText) view.findViewById(R.id.username);
+//        String s = e.getText().toString();
+        goToActivity2();
+    }
+
+    public void goToActivity2() {
+        Intent intent = new Intent(this, Main2Activity.class);
+//        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
