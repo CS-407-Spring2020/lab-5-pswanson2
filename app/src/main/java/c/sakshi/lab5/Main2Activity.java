3bc9845e3de56452b7c9182315fe1cb00b82f774
Peter <pswanson2@wisc.edu>
2020-02-29 12:15:54 -0600

    Added a main menu and logout feature

--- app/src/main/java/c/sakshi/lab5/Main2Activity.java
+++ app/src/main/java/c/sakshi/lab5/Main2Activity.java
@@ -1,9 +1,15 @@
 package c.sakshi.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
     TextView t;
@@ -15,6 +21,27 @@ public class Main2Activity extends AppCompatActivity {
 
         t = (TextView) findViewById(R.id.textView2);
         String s = getIntent().getStringExtra("message");
-        t.setText(s);
+        t.setText("Hello, " + s);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Functionality not implemented", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
     }
 }
+
--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 
@@ -29,4 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
         e = (EditText) findViewById(R.id.username);
     }
+
+
 }
